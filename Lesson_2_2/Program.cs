21954c24a36b802b5a2ad6718b5b211944b39cb0
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-08 16:40:00 +0400

    Lesson 2 Exercise 2

---
+++ Lesson_2_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lesson_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mothNumber = parseNumber();
+            var mothName = getMothByNumber(mothNumber);
+
+            Console.WriteLine($"Название месяца: {mothName}");
+        }
+
+        static string getMothByNumber(int monthNumber)
+        {
+            switch (monthNumber)
+            {
+                case 1:
+                    return "Январь";
+                case 2:
+                    return "Ферлаь";
+                case 3:
+                    return "Март";
+                case 4:
+                    return "Апрель";
+                case 5:
+                    return "Май";
+                case 6:
+                    return "Июнь";
+                case 7:
+                    return "Июль";
+                case 8:
+                    return "Август";
+                case 9:
+                    return "Сентабрь";
+                case 10:
+                    return "Октабрь";
+                case 11:
+                    return "Ноябрь";
+                case 12:
+                    return "Декабрь";
+                default:
+                    return null;
+            }
+        }
+
+        static int parseNumber()
+        {
+            int parsingNumber;
+
+            Console.Write("Введите номер месяца: ");
+            while (!int.TryParse(Console.ReadLine(), out parsingNumber))
+            {
+                Console.Write("Введите целове число от 1 до 12: ");
+            }
+
+            return parsingNumber;
+        }
+    }
+}
