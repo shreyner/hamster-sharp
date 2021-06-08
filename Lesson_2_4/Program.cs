9a783fbd065421304b8399bc7ff9859eb0ddf1a5
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-08 18:21:03 +0400

    Lesson 2 Exercise 4

---
+++ Lesson_2_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orders = new[]
+            {
+                new {name = "Шампунь", amount = 1, price = 233},
+                new {name = "Кондиционер", amount = 1, price = 233},
+                new {name = "хлеб", amount = 2, price = 233}
+            };
+            DateTime dateNow = DateTime.Now;
+            string shopName = "Base Shop";
+
+            Console.WriteLine("Чек");
+            Console.WriteLine($"Название магазина: {shopName}");
+            Console.WriteLine($"Дата: {dateNow}");
+            Console.WriteLine("=====");
+
+            Console.WriteLine("№, Name, amount, price");
+
+            for (var i = 0; i < orders.Length; i += 1)
+            {
+                var order = orders[i];
+                Console.WriteLine($"#{i} {order.name} {order.amount} {order.price}");
+            }
+
+            Console.WriteLine("=====");
+
+            var sum = orders.Aggregate(0, (acc, order) => acc += order.amount * order.price);
+
+            Console.WriteLine($"Итог: {sum}");
+        }
+    }
+}
