cf5586563227f13d04b2206b922f25e33ea06743
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-10 16:08:19 +0400

    Lesson 3 Exercise 3

---
+++ Lesson_3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            var someString = Console.ReadLine();
+            var reverseString = string.Join("", someString.ToArray().Reverse());
+
+
+            Console.WriteLine($"Введенная строка на оборот: {reverseString}");
+        }
+    }
+}
