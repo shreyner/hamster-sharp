e1c9e057b3561bd058349905d5f3df4a22432cc6
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-10 15:05:13 +0400

    Lesson 3 Exercise 1

---
+++ Lesson_3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array =
+            {
+                new[] {1, 2, 3},
+                new[] {4, 5, 6},
+                new[] {7, 8, 9}
+            };
+
+            for (int i = 0; i < array.Length; i += 1)
+            {
+                Console.WriteLine($"Элемент из массива с кардинатами [{i}, {i}]: {array[i][i]}");
+            }
+        }
+    }
+}
