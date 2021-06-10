1daae407aa7602d3f96217494c13e8215c81b6c9
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-10 15:24:21 +0400

    Lesson 3 Exercise 2

---
+++ Lesson_3_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contactList = new string[5, 2]
+            {
+                {"Mrs. Alvin Gusikowski", "+1 (592) 461-0639"},
+                {"Terry King", "+1 (641) 216-3431"},
+                {"Brenda Bode", "+1 (572) 438-9636"},
+                {"Mr. Derrick Zulauf", "+1 (354) 631-5839"},
+                {"Stanley Brakus", "+1 (250) 605-1191"},
+            };
+        }
+    }
+}
