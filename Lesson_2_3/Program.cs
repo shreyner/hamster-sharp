be9ec69708f42b82e1c8a3ba58ec50f357efc7d6
Aleksandr Shreyner <9334650+shreyner@users.noreply.github.com>
2021-06-11 11:40:20 +0300

    Lesson 2 Exercise 3 (#5)

---
+++ Lesson_2_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = parseNumber();
+
+            Console.WriteLine((number % 2) == 0 ? "Введенное число четное" : "Введенное число не четное");
+        }
+
+        static int parseNumber()
+        {
+            int parsedNumber;
+
+            do
+            {
+                Console.WriteLine("Введите целое число: ");
+            } while (!int.TryParse(Console.ReadLine(), out parsedNumber));
+
+            return parsedNumber;
+        }
+    }
+}
