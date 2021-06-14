b1b2373ad33106379859699840ab82d8d19da222
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-14 19:40:09 +0400

    Lesson 3 Exercise 4*

---
+++ Lesson_3_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new char[10, 10]
+            {
+                {'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X', 'O'},
+                {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                {'O', 'O', 'X', 'X', 'X', 'X', 'O', 'O', 'O', 'O'},
+                {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X', 'X'},
+                {'O', 'O', 'X', 'O', 'O', 'X', 'O', 'O', 'O', 'O'},
+                {'X', 'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                {'O', 'O', 'X', 'O', 'O', 'O', 'X', 'X', 'O', 'O'},
+                {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                {'O', 'O', 'X', 'X', 'O', 'O', 'X', 'O', 'O', 'O'},
+                {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+            };
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int column = 0; column < board.GetLength(1); column++)
+                {
+                    Console.Write($"{board[row, column]} ");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
