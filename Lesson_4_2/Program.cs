5beb7486631a6e5f43943b380e2521c8b9fe688b
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-21 23:52:51 +0300

    Lesson 4 Exercise 2

---
+++ Lesson_4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числа через пробел: ");
+
+            var enterString = Console.ReadLine();
+
+            var sum = enterString
+                .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).Sum();
+
+            Console.Write($"Сумма всех чиссел: {sum}");
+        }
+    }
+}
