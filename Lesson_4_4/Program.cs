c4ff10039b304efdc7df655b520cd6ca8eaf629a
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-22 23:27:07 +0300

    Lesson 4 Exercise 4

---
+++ Lesson_4_4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lesson_4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = ConsoleReadNumber();
+
+            Console.WriteLine($"Числа фибоначи: {Fib(number)}");
+        }
+
+        static int Fib(int num)
+        {
+            if (num is 0 or 1)
+            {
+                return num;
+            }
+
+            if (num < 0) // для отрицательных
+            {
+                return Fib(num + 2) - Fib(num + 1);
+            }
+
+            return Fib(num - 1) + Fib(num - 2);
+        }
+
+        static int ConsoleReadNumber()
+        {
+            int parsedNumber;
+            Console.Write("Введите натуральное число: ");
+            while (!int.TryParse(Console.ReadLine(), out parsedNumber))
+            {
+                Console.Write("Введите натуральное число: ");
+            }
+
+            return parsedNumber;
+        }
+    }
+}
