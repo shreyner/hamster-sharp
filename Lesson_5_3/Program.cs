adcf79e944377aecaa022ce7a0f05b599d6e8309
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-23 22:02:34 +0300

    Lesson 5 Exercise 3

---
+++ Lesson_5_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte someNumber;
+            do
+            {
+                Console.Write("Введите число от 0 до 255: ");
+            } while (!byte.TryParse(Console.ReadLine(), out someNumber));
+
+
+            File.WriteAllBytes("./binary.bin", new[] {someNumber});
+        }
+    }
+}
