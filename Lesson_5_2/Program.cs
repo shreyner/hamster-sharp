53a8cb2daa68a436bfa4d6c16a65a23862a5a301
Aleksandr Shreyner <9334650+shreyner@users.noreply.github.com>
2021-06-25 19:27:42 +0300

    Lesson 5 Exercise 2 (#19)
    * Lesson 5 Exercise 2
    
    * add empty string

---
+++ Lesson_5_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateNow = DateTime.UtcNow;
+            var fileName = "./currentTime.txt";
+
+            File.AppendAllText(fileName, $"{dateNow.ToString("O")}\n");
+        }
+    }
+}
