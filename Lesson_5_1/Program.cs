78313cd5e008aa2aa39b02cf6732681894551139
Aleksandr Shreyner <9334650+shreyner@users.noreply.github.com>
2021-06-25 19:27:33 +0300

    Lesson 5 Exercise 1 (#18)

---
+++ Lesson_5_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lesson_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст для записи в файл: ");
+            var someText = Console.ReadLine();
+
+            var fileName = "./text.txt";
+            File.WriteAllText(fileName, someText, Encoding.UTF8);
+        }
+    }
+}
