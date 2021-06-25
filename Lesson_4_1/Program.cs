ce693b85624ea038bded4341289ed78bcc17f095
Aleksandr Shreyner <9334650+shreyner@users.noreply.github.com>
2021-06-25 19:26:25 +0300

    Lesson 4 Exercise 1 (#14)

---
+++ Lesson_4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFullName("Светлана", "Шарова", "Иванова"));
+            Console.WriteLine(GetFullName("Антонида", "Мясникова", "Владимировна"));
+            Console.WriteLine(GetFullName("Степан", "Нестеров", "Вадимов"));
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{lastName} {firstName} {patronymic}";
+        }
+    }
+}
