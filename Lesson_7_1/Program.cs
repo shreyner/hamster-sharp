702e183d6ad0930165ad9dccaa76f8dad6b15350
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-27 13:27:00 +0300

    Lesson 7 Exercise 1

---
+++ Lesson_7_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = "some secret password";
+            Console.WriteLine("Enter password:");
+            var input = Console.ReadLine();
+            Console.WriteLine("After decompile");
+            if (input == secret)
+            {
+                Console.WriteLine("Welcome!");
+                return;
+            }
+        }
+    }
+}
