bb27a9f45353a7e44c9c518166b8058e49144658
Aleksandr Shreyner <9334650+shreyner@users.noreply.github.com>
2021-07-02 20:01:28 +0300

    Lesson 7 Exercise 1 (#22)
    * Lesson 7 Exercise 1
    
    * remove console write

---
+++ Lesson_7_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = "some secret password";
+            Console.WriteLine("Enter password:");
+            var input = Console.ReadLine();
+            if (input == secret)
+            {
+                Console.WriteLine("Welcome!");
+                return;
+            }
+        }
+    }
+}
