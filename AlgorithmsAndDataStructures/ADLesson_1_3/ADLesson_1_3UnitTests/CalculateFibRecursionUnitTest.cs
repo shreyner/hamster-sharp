9cdc92c52933374429f4cc41e38058ad2dda846c
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-07-06 22:57:59 +0300

    Add calculate Fib Loop

--- AlgorithmsAndDataStructures/ADLesson_1_3/ADLesson_1_3UnitTests/CalculateFibRecursionUnitTest.cs
+++ AlgorithmsAndDataStructures/ADLesson_1_3/ADLesson_1_3UnitTests/CalculateFibRecursionUnitTest.cs
@@ -7,6 +7,17 @@ namespace ADLesson_1_3UnitTests
     public class UnitTest1
     {
         [Fact]
+        public void TestCalculateFibRecursionWith0()
+        {
+            var input = 0;
+            var expected = 0;
+
+            var actual = Fibonacci.Recursion(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void TestCalculateFibRecursionWith1()
         {
             var input = 1;
@@ -46,6 +57,60 @@ namespace ADLesson_1_3UnitTests
             var expected = 13;
 
             var actual = Fibonacci.Recursion(input);
+
+            Assert.Equal(expected, actual);
+        }
+        
+        public void TestCalculateFibLoopWith0()
+        {
+            var input = 0;
+            var expected = 0;
+
+            var actual = Fibonacci.Loop(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestCalculateFibLoopWith1()
+        {
+            var input = 1;
+            var expected = 1;
+
+            var actual = Fibonacci.Loop(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestCalculateFibLoopWith2()
+        {
+            var input = 2;
+            var expected = 1;
+
+            var actual = Fibonacci.Loop(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestCalculateFibLoopWith3()
+        {
+            var input = 3;
+            var expected = 2;
+
+            var actual = Fibonacci.Loop(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestCalculateFibLoopWith7()
+        {
+            var input = 7;
+            var expected = 13;
+
+            var actual = Fibonacci.Loop(input);
 
             Assert.Equal(expected, actual);
         }
--- AlgorithmsAndDataStructures/ADLesson_1_3/Fibonacci.cs
+++ AlgorithmsAndDataStructures/ADLesson_1_3/Fibonacci.cs
@@ -11,5 +11,23 @@ namespace ADLesson_1_3
 
             return Recursion(num - 1) + Recursion(num - 2);
         }
+
+        public static int Loop(int num)
+        {
+            if (num is 0 or 1) return num;
+
+            var fb1 = 1;
+            var fb2 = 1;
+
+            for (int i = 2; i < num; i++)
+            {
+                var sum = fb1 + fb2;
+
+                fb1 = fb2;
+                fb2 = sum;
+            }
+
+            return fb2;
+        }
     }
 }
