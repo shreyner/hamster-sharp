1aa68a5bedf71d6b145cf5a09b41c1e4d2804da1
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-08-01 16:07:29 +0300

    [Algorithms And Data Structures] Lesson 8 Exercise 1

---
+++ AlgorithmsAndDataStructures/ADLesson_8_1/ADLesson_8_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADLesson_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNotSorted = new List<int>(new[] {0, 5, 6, 3, 8, 4, 2, 1, 7, 9});
+
+            Console.WriteLine("Input: {0}\nResult: {1}",
+                string.Join(
+                    ", ",
+                    inputNotSorted),
+                string.Join(
+                    ", ",
+                    SortHelpers.BubbleSort(inputNotSorted))
+            );
+        }
+    }
+}
