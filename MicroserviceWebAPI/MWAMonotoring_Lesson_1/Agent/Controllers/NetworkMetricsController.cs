414e419afae5f69f13b38883bd4dcf7d0951b799
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-09-05 20:04:11 +0300

    [Microservice Web API] Add endpoint for get metrics from Agent service

---
+++ MicroserviceWebAPI/MWAMonotoring_Lesson_1/Agent/Controllers/NetworkMetricsController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agent.Controllers
+{
+    [Route("api/network")]
+    [ApiController]
+    public class NetworkMetricsController : ControllerBase
+    {
+        [HttpGet("from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetrics([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok(Enumerable.Empty<int>());
+        }
+    }
+}
