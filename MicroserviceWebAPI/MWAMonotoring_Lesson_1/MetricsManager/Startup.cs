eb76679da283b00d01cc71e407920e8d594076a3
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-09-12 16:53:58 +0300

    [Microservice Web API] Lesson 2: Add package Scrutor

--- MicroserviceWebAPI/MWAMonotoring_Lesson_1/MetricsManager/Startup.cs
+++ MicroserviceWebAPI/MWAMonotoring_Lesson_1/MetricsManager/Startup.cs
@@ -27,6 +27,7 @@ namespace MetricsManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Scrutor
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
