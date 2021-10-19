90c54dbeb212d91cf23bd55453778850f8599fc1
Aleksandr Shreyner <9334650+shreyner@users.noreply.github.com>
2021-10-19 21:08:40 +0300

    [OOP] Lesson 2 (#42)
    * [OOP] Lesson 2 Exercise 1
    
    * [OOP] Lesson 2 Exercise 2
    
    * [OOP] Lesson 2 Exercise 3
    
    * [OOP] Lesson 2 Exercise 4

---
+++ OOP/OOPLesson2/Account.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace OOPLesson2 {
+
+    enum AccountType
+    {
+        Debit,
+        Credit
+    }
+
+    class Account
+    {
+        static long LastNumber  = 0;
+        public long Number {get;} = 0;
+        public AccountType Type {get; set;} = AccountType.Debit;
+        public decimal Amount {get; set;} = 0;
+
+        public Account() {
+            Number = Account.GenerateAccountNumber();
+        }
+
+        public Account(AccountType type) {
+            Number = Account.GenerateAccountNumber();
+            Type = type;
+        }
+
+        public Account(decimal amount) {
+            Number = Account.GenerateAccountNumber();
+            Amount = amount;
+        }
+
+        public Account(AccountType type, decimal amount) {
+            Number = Account.GenerateAccountNumber();
+        }
+
+        public override string ToString()
+        {
+            return $"[{base.ToString()}]: Number: {Number}, AccountType: {Type}, Amount: {Amount}";
+        }
+
+        static public long GenerateAccountNumber() {
+            return Account.LastNumber++;
+        }
+    }
+}
---
+++ OOP/OOPLesson2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOPLesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new Account(AccountType.Credit, Convert.ToDecimal(23333.23));
+            account1.Type= AccountType.Credit;
+            account1.Amount = Convert.ToDecimal(23333.23);
+
+            var account2 = new Account(AccountType.Debit);
+            account2.Amount = Convert.ToDecimal(3333);
+
+            Console.WriteLine("Exercise 4");
+            Console.WriteLine(account1);
+            Console.WriteLine(account2);
+        }
+    }
+}
