5c22f3649876565139f00e69bf7f231503822f1b
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-10-19 22:43:56 +0300

    [OOP] Lesson 3 Exercise 1

--- OOP/OOPLesson2/Program.cs
+++ OOP/OOPLesson2/Program.cs
@@ -13,9 +13,14 @@ namespace OOPLesson2
             var account2 = new Account(AccountType.Debit);
             account2.Amount = Convert.ToDecimal(3333);
 
-            Console.WriteLine("Exercise 4");
-            Console.WriteLine(account1);
-            Console.WriteLine(account2);
+            Console.WriteLine("Lessont 3 Exercise 1");
+            Console.WriteLine("Before transaction");
+            Console.WriteLine($"Account1: {account1}");
+            Console.WriteLine($"Account2: {account2}");
+            Console.WriteLine("After transaction form account1 to account2 300 amount");
+            account2.Transfer(account1, 300);
+            Console.WriteLine($"Account1: {account1}");
+            Console.WriteLine($"Account2: {account2}");
         }
     }
 }
